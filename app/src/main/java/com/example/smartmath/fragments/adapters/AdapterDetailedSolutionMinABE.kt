824e3b7ceb85bba1616a86b3cd0296e76396657d
ionnomirai <kindbakaneko@gmail.com>
2024-11-07 05:53:52 -0800

    Added display of GoldenSection method (detailed):
    - added correct display of detailed solution for GoldenSection method;
    - formatted code in AdapterDetailedSolutionMinABE.

--- app/src/main/java/com/example/smartmath/fragments/adapters/AdapterDetailedSolutionMinABE.kt
+++ app/src/main/java/com/example/smartmath/fragments/adapters/AdapterDetailedSolutionMinABE.kt
@@ -26,33 +26,62 @@ class AdapterDetailedSolutionMinABE(
     }
 
     override fun onBindViewHolder(holder: AdapterDetailedViewHolder, position: Int) {
-        when (method) {
-            MethodNames.Dichotomy -> holder.setDataDichotomy(data[position])
-            MethodNames.GoldenSection -> holder.setDataGS()
-        }
+        holder.setData(data[position], method)
     }
 
     override fun getItemCount(): Int = data.size
 
+    // ------------------------------------------ Nested  class -------------------------------------
+
     class AdapterDetailedViewHolder(val innerBinding: CardRvDetailedSolutionMinAbeBinding) :
         RecyclerView.ViewHolder(innerBinding.root) {
 
-
-
-        fun setDataDichotomy(el: StateMinABE) {
+        fun setData(el: StateMinABE, methodName: MethodNames) {
             val position = this.adapterPosition.toString() // current position in the data list
             innerBinding.apply {
-                tvStep.text = getAdjustedText(R.string.title_step, ":", position)
+                setCommonFields(el, position) /* Set data for both Dichotomy and GoldenSection.*/
+                when(methodName){
+                    MethodNames.Dichotomy -> setDataDichotomy(el, position)
+                    MethodNames.GoldenSection -> setDataGS()
+                }
+            }
+        }
+
+        /* Set data specific to Dichotomy */
+        private fun setDataDichotomy(el: StateMinABE, step: String){
+            innerBinding.apply {
+                tvMedium.text = getAdjustedText(R.string.x_midpoint, "=", el.middle.toString())
+
+                tvAStepPlusOne.text = getAdjustedText(
+                    R.string.char_plus_one_plus_num, "=", "a", step, el.aPlOne.toString()
+                )
+                tvBStepPlusOne.text = getAdjustedText(
+                    R.string.char_plus_one_plus_num, "=", "b", step, el.bPlOne.toString()
+                )
+            }
+        }
+
+        /* Set data specific to GoldenSection */
+        private fun setDataGS() {
+            innerBinding.apply {
+                tvMedium.visibility = View.GONE
+                tvAStepPlusOne.visibility = View.GONE
+                tvBStepPlusOne.visibility = View.GONE
+            }
+        }
+
+        /* Fill in the data common to all methods. */
+        private fun setCommonFields(el: StateMinABE, step: String){
+            innerBinding.apply {
+                tvStep.text = getAdjustedText(R.string.title_step, ":", step)
 
                 tvAStep.text = getAdjustedText(
-                    R.string.char_plus_num, "=", "a", position, el.a.toString()
+                    R.string.char_plus_num, "=", "a", step, el.a.toString()
                 )
 
                 tvBStep.text = getAdjustedText(
-                    R.string.char_plus_num, "=", "b", position, el.b.toString()
+                    R.string.char_plus_num, "=", "b", step, el.b.toString()
                 )
-
-                tvMedium.text = getAdjustedText(R.string.x_midpoint, "=", el.middle.toString())
 
                 tvXFirst.text = getAdjustedText(
                     R.string.char_plus_num, "=", "x", "1", el.x1.toString()
@@ -72,16 +101,8 @@ class AdapterDetailedSolutionMinABE(
 
                 el.fX1LessThanFX2?.let { tvFMatch.text = if(it) "F1 < F2" else "F2 >= F2" }
 
-                tvAStepPlusOne.text = getAdjustedText(
-                    R.string.char_plus_one_plus_num, "=", "a", position, el.aPlOne.toString()
-                )
-
-                tvBStepPlusOne.text = getAdjustedText(
-                    R.string.char_plus_one_plus_num, "=", "b", position, el.bPlOne.toString()
-                )
-
                 tvCondition.text = getAdjustedText(
-                    R.string.condition_detailed, "=", position, el.condition.toString()
+                    R.string.condition_detailed, "=", step, el.condition.toString()
                 )
 
                 if (el.xEnd != null){
@@ -98,13 +119,10 @@ class AdapterDetailedSolutionMinABE(
                         setColorFilter(itemView.context.getColor(R.color.juicy_green))
                     }
                 }
-
-
             }
         }
 
-        fun setDataGS() {}
-
+        /* Format and return the final string for display. */
         private fun getAdjustedText(@StringRes stringId: Int, divider: String, vararg values: String): SpannableString {
             val text = itemView.context.getString(stringId, *values)
             val indexDiv = text.indexOf(divider) + 1
